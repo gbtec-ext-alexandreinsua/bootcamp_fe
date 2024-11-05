64945a49b7ec112c6b8d75f11c68bda17ad6f8f6
Roi Oliveira Outeda <roioliveira@gmail.com>
2024-11-05 08:50:13 +0100

    mis ejercicios

--- ejercicios/javascript/03-bucles-itreraciones/03-bucles-iteraciones.js
+++ ejercicios/javascript/03-bucles-itreraciones/03-bucles-iteraciones.js
@@ -4,6 +4,24 @@
   Un número es primo si solo es divisible por 1 y por sí mismo.
   El método debe validar el tipo de dato y mostrar el resultado en la consola.
 */
+function esNumeroPrimo(numero) {
+  if (numero <= 1) {
+      console.log("Por favor, ingresa un número mayor que 1.");
+      return false;
+    }
+
+  for (let i = 2; i < numero; i++) {
+    if (numero % i === 0) {
+      console.log(`${numero} no es un número primo.`);
+      return;
+    }
+  }
+  console.log(`${numero} es un número primo.`);
+  return;
+}
+
+esNumeroPrimo(9); 
+esNumeroPrimo(18); 
 
 /*
   Ejercicio 5: Calculadora Básica
@@ -12,3 +30,46 @@
   Debe validar que los dos primeros parámetros sean números y que la operación sea válida.
   Si la operación es una división, debe verificar que el segundo número no sea cero.
 */
+
+
+function calculadora(num1, num2, operacion) {
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+      console.log("Error: Los primeros dos parámetros deben ser números.");
+      return;
+  }
+
+  let resultado;
+
+  switch (operacion) {
+    case 'suma':
+      resultado = num1 + num2;
+      break;
+    case 'resta':
+      resultado = num1 - num2;
+      break;
+    case 'multiplicacion':
+      resultado = num1 * num2;
+      break;
+    case 'division':
+      if (num2 === 0) {
+        console.log("No se puede dividir por cero.");
+        
+      }else
+      resultado = num1 / num2;
+      break;
+    default:
+      console.log("Error: Operación no válida. Usa 'suma', 'resta', 'multiplicación' o 'división'.");
+  }
+     
+  }
+  if(resutlado || resultado === 0){
+      console.log(`El resultado de la ${operacion} es: ${resultado}`);
+  }else{
+      console.log("Error: la operacion")
+  }
+calculadora(31, 52, 'suma'); 
+calculadora(31, 52, 'suma');
+calculadora(105, 14, 'resta');
+calculadora(6, 5, 'multiplicacion');
+calculadora(30, 5, 'division');     
+calculadora(10, 0, 'division');     
---
+++ ejercicios/javascript/04-arrays/04-arrays.js
@@ -0,0 +1,47 @@
+/* Dado el siguiente array
+
+const myArray = [42, "hola", 3, true, 76, 19, false, "JavaScript", 88, 15,null, 34, 53, 91, 12, "mundo", 27, 64, 82, 39 ];
+
+1. Halla la suma de los numeros que contiene e múestrala por consola.
+
+2. Si el elemento es un string, muéstralo por pantalla.
+
+3. Si el elemento no es un número o un string, ignóralo */
+ 
+const myArray = [42, "hola", 3, true, 76, 19, false, "JavaScript", 88, 15, null, 34, 53, 91, 12, "mundo", 27, 64, 82, 39];
+
+let suma = 0;
+//element => es una función flecha que se utiliza para definir 
+//una función que se ejecutará para cada elemento de myArray cuando se llama a forEach.
+
+myArray.forEach(elemento => {
+    
+    if (typeof elemento === 'number') {
+        suma += elemento;
+        
+    }
+    else if (typeof elemento ==='string') {
+        console.log(elemento); 
+    }else{}
+    
+});
+
+console.log("La suma de los números es:", suma);
+
+
+//Ejemplo 2
+let suma2 = 0;
+function array2(array2){
+    for(let i =0 ; i < array2.length; i++){
+        if (typeof array2[i] === 'number'){
+            suma2 += array2[i]
+            
+        
+        }else if( typeof array2[i] === 'string'){
+            console.log(array2[i])
+        } 
+    }
+    console.log("La suma de los numeros dentro del array es : ",suma2)
+}
+array2(myArray)
+
