273dbd6621a0c9a01b1f4397f5336921f9b584af
Jorge338991 <jorge338991@gmail.com>
2024-11-05 22:14:54 +0100

    Se añade solucion a funciones con arrays

---
+++ ejercicios/javascript/04-arrays/04-arrays.js
@@ -0,0 +1,41 @@
+/* Dado el siguiente array
+const myArray = [42, "hola", 3, true, 76, 19, false, "JavaScript", 88, 15,null, 34, 53, 91, 12, "mundo", 27, 64, 82, 39 ];
+1. Halla la suma de los numeros que contiene e múestrala por consola.
+2. Si el elemento es un string, muéstralo por pantalla.
+3. Si el elemento no es un número o un string, ignóralo */
+
+const myArray = [42, "hola", 3, true, 76, 19, false, "JavaScript", 88, 15, null, 34, 53, 91, 12, "mundo", 27, 64, 82, 39];
+
+function array(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === "number") {
+      sum += array[i]; // Suma los números
+    } else if (typeof array[i] === "string") {
+      console.log(array[i]); // Muestra los strings
+    }
+    // Ignora los elementos que no son ni número ni string
+  }
+
+  console.log("La suma de los números es:", sum);
+}
+array(myArray);
+
+function array2(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === "number") {
+      sum += array[i]; // Suma los números
+    } else if (typeof array[i] === "string") {
+      console.log(array[i]); // Muestra los strings
+    }
+    // Ignora los elementos que no son ni número ni string
+  }
+  return sum;
+}
+
+function indiferente() {
+  const arrayNum = [1, 2, 3, 4, 5, "lalala"];
+  console.log(array2(arrayNum));
+}
+indiferente();
