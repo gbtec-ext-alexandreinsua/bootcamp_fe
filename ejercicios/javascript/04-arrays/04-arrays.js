379a792141d62e1d5a55154ee9e78ee52c04f6c7
rubbeen99 <ruben20042000@gmail.com>
2024-11-08 10:02:39 +0100

    Añadidos cambios en arrays.js y nuevos archivos CSS

--- ejercicios/javascript/04-arrays/04-arrays.js
+++ ejercicios/javascript/04-arrays/04-arrays.js
@@ -1,22 +1,23 @@
-/*
-EJERCICIO 21.- 
-Dado el siguiente array
+/* Dado el siguiente array
 const myArray = [42, "hola", 3, true, 76, 19, false, "JavaScript", 88, 15,null, 34, 53, 91, 12, "mundo", 27, 64, 82, 39 ];
- 1. Halla la suma de los numeros que contiene e múestrala por consola.
- 2. Si el elemento es un string, muéstralo por pantalla.
- 3. Si el elemento no es un número o un string, ignóralo */
-
- const myArray = [42, "hola", 3, true, 76, 19, false, "JavaScript", 88, 15,null, 34, 53, 91, 12, "mundo", 27, 64, 82, 39 ];
+1. Halla la suma de los numeros que contiene y múestrala por consola.
+2. Si el elemento es un string, muéstralo por pantalla.
+3. Si el elemento no es un número o un string, ignóralo. */
+// Dado el siguiente array
+const myArray = [42, "hola", 3, true, 76, 19, false, "JavaScript", 88, 15, null, 34, 53, 91, 12, "mundo", 27, 64, 82, 39];
 
 let suma = 0;
-for (let i = 0; i < myArray.length; i++) {
-  if (typeof myArray[i] === "string") {
-    console.log(myArray[i]);
-  }
 
-  if (typeof myArray[i] === "number") {
-    suma += myArray[i];
-  }
+for (let i = 0; i < myArray.length; i++) {
+    const elemento = myArray[i]; // Obtener el elemento actual
+
+    if (typeof elemento === "number") {
+        suma += elemento;
+    }
+    else if (typeof elemento === "string") {
+        console.log("String encontrado:", elemento);
+    }
 }
 
-console.log("La suma del array es ", suma);
+// Mostramos la suma de los números por consola
+console.log("La suma de los números es:", suma);
