9bfcd6f3730065e8077f2616d1633ba6146c9985
Edugonzalez <eduu.gonzalez@gmail.com>
2024-11-13 10:05:18 +0100

    ejercicios updated

--- ejercicios/javascript/01-funciones/01-funciones.js
+++ ejercicios/javascript/01-funciones/01-funciones.js
@@ -56,13 +56,13 @@ function calculateSecondsToHumanVersion(seconds) {
   const remainSeconds = Number.parseInt(seconds % secondsInMinute);
   console.log(
     seconds +
-      " segundos son " +
-      hours +
-      " horas " +
-      minutes +
-      " minutos " +
-      remainSeconds +
-      "  segundos"
+    " segundos son " +
+    hours +
+    " horas " +
+    minutes +
+    " minutos " +
+    remainSeconds +
+    "  segundos"
   );
 }
 
@@ -78,13 +78,13 @@ function calculateSecondsToHumanVersion2(seconds) {
   const remainSeconds = Math.floor(seconds % secondsInMinute);
   console.log(
     seconds +
-      " segundos son " +
-      hours +
-      " horas " +
-      minutes +
-      " minutos " +
-      remainSeconds +
-      "  segundos"
+    " segundos son " +
+    hours +
+    " horas " +
+    minutes +
+    " minutos " +
+    remainSeconds +
+    "  segundos"
   );
 }
 
@@ -137,15 +137,59 @@ y muestre por consola los ingredientes que se necesitan para hacer una
 tortilla de patatas. Por cada comensal se necesitan 200 gr de patatas.
 Por cada kilo de patatas se necesitan 5 huevos y 100 g de cebollas.
 Por ejemplo: Para 5 comensales debe mostrar por pantalla
-"Para una tortila de 5 comensales se necesitan 1 kg de patatas, 5 huevos y 500 g de cebolla" 
+"Para una tortila de 5 comensales se necesitan 1 kg de patatas, 5 huevos y 100 g de cebolla" 
 */
 
+function showIngredientes() {
+  //Para eliminar espacios en balanco o tabulaciones o saltos de línea que producirían errores usamos el método .trim()
+  let num = Number(prompt("Para cuandos vas a cocinar?").trim());
+  let potato = num * 200;
+  let onion = num * 20;
+  let eggs = num;
+
+  if (!num && !num === 0 || num < 0) {
+    alert(" Introduce un número válido.")
+  }
+  //Number.isInteger para comprobar que es un número entero y Nuymber() para convertir la cadena en un número.
+  else if (Number.isNaN(num)) {
+    alert("Introduce un número, por favor.")
+  }
+  else if (num === 0) {
+    alert("Como vas a cocinar para 0, panoli?")
+  }
+  else if (!Number.isInteger(num)) {
+    alert("No puedes introducir un número decimal.")
+  }
+  else {
+    console.log("Para una tortila de " + num + " comensales se necesitan: " + potato + " kg de patatas, " + eggs + " huevos y " + onion + " g de cebolla")
+  }
+}
+//showIngredientes()
 /*
 EJERCICIO 5.- 
 /* Crea un método que le pida al usuario 3 números enteros y
 muestre por pantalla la media de los 3.
 "La media de 1, 2 y 3 es 2."
 */
+function pedir3Enteros() {
+  //const numeros = [];
+  const numeros = new Array();
+
+  do {
+    const respuesta = prompt("Introduce un número entero por favor").trim();
+    const num = Number(respuesta);
+    if (!respuesta || Number.isNaN(num)) {
+      window.alert("Introduce un número válido");
+    } else {
+      numeros.push(num);
+    }
+  } while (numeros.length === 3) {
+    window.alert("La media de dichos números es: " + (numeros[0] + numeros[1] + numeros[2]) / 3)
+  }
+}
+
+//pedir3Enteros()
+
 
 /*
 EJERCICIO 6.- 
@@ -154,3 +198,53 @@ y los litros consumidos en un viaje. Debe mostrar por consola el
 consumo de combustible por km y el coste total del viaje a un coste
 de 0.21€ por litro y kilómetro.
 */
+
+function calcularCoste() {
+  const precio = 0.21;
+  const zero = 0;
+  let kmValidos = false;
+  let ltvalidos = false;
+  let kilometros = 0;
+  let litros = 0;
+  
+  const consumo = litros / kilometros;
+  const costeViaje = consumo * precio;
+  do {
+    const kmstring = window.prompt("Introduce los kilometros recorridos");
+    //!km revisa cadenas vacías, 
+    let kilometros = validarNumeros(kmstring, "Debes introducir los kilometros recorridos");
+    kmValidos = zero < kilometros;
+    if (!kmValidos) {
+      window.alert("Introduce un número de kilometros válido")
+    }
+  } while (!kmValidos);
+  do {
+    const ltstring = window.prompt("Introduce los litros consumidos");
+    let litros = validarNumeros(ltstring, "Debes introducir los litros repostados");
+    ltvalidos = zero < litros;
+    if (!ltvalidos) {
+      window.alert("Introduce un número de litros válido")
+    }
+  } while (!ltvalidos);
+
+  window.alert(`El consumo es ${consumo} por cada kilómetro, y el coste total es ${costeViaje}`)
+}
+
+calcularCoste();
+
+
+function validarNumeros(string, mensaje) {
+  let resultado;
+  if (!string) {
+    window.alert(mensaje)
+  }
+  else {
+    resultado = Number(string)
+    if (Number.isNaN(resultado)) {
+      window.alert("Introduce un número")
+    } else {
+      return resultado;
+    }
+  }
+}
+
