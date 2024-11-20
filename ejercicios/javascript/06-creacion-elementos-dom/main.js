d3a5529861ab9bd899de546d7ee8cce70a774a8f
Jorge338991 <jorge338991@gmail.com>
2024-11-20 22:32:51 +0100

    se añade solucion a 06-creacion-elementos-dom desde fichero .js

--- ejercicios/javascript/06-creacion-elementos-dom/main.js
+++ ejercicios/javascript/06-creacion-elementos-dom/main.js
@@ -15,7 +15,42 @@ const productos = [
     descripcionImagen: "Producto 1",
     nombre: "Producto 1",
     descripcion: "Descripción del producto 1",
-    precio: 10,
+    precio: "$10",
+  },
+  {
+    imagen: "./images/producto2.jpg",
+    descripcionImagen: "Producto 2",
+    nombre: "Producto 2",
+    descripcion: "Descripción del producto 2",
+    precio: "20€",
+  },
+  {
+    imagen: "./images/producto3.jpg",
+    descripcionImagen: "Producto 3",
+    nombre: "Producto 3",
+    descripcion: "Descripción del producto 3",
+    precio: "30€",
+  },
+  {
+    imagen: "./images/producto4.jpg",
+    descripcionImagen: "Producto 4",
+    nombre: "Producto 4",
+    descripcion: "Descripción del producto 4",
+    precio: "24€",
+  },
+  {
+    imagen: "./images/producto5.jpg",
+    descripcionImagen: "Producto 5",
+    nombre: "Producto 5",
+    descripcion: "Descripción del producto 5",
+    precio: "250€",
+  },
+  {
+    imagen: "./images/producto6.jpg",
+    descripcionImagen: "Producto 6",
+    nombre: "Producto 6",
+    descripcion: "Descripción del producto 6",
+    precio: "15€",
   },
 ];
 
@@ -23,14 +58,31 @@ const $wrapper = document.querySelector(".card-wrapper");
 console.log($wrapper);
 
 
-/*
-<!-- <div class="card">
-<a href="#">
-  <img src="./images/producto1.jpg" alt="Producto 1" />
-  <h3>Producto 1</h3>
-  <p>Descripción del producto 1.</p>
-  <p>Precio: $10</p>
-</a>
-</div>
-*/
+productos.forEach(producto => {
+  
+  const $div = document.createElement("div");
+  $div.className="card";
+  
+  const $a = document.createElement("a");
+  $a.href = "#";
+  
+  const $img = document.createElement("img");
+  $img.src = producto.imagen;
+  $img.alt = producto.descripcionImagen;
 
+  const $h3 = document.createElement("h3");
+  $h3.textContent =  producto.nombre;
+  
+  const $descripcion = document.createElement("p");
+  $descripcion.innerText = producto.descripcion;
+  
+  const $precio = document.createElement("p");
+  $precio.innerText = producto.precio;
+  
+  $wrapper.appendChild($div);
+  $div.appendChild($a);
+  $a.appendChild($img);
+  $a.appendChild($h3);
+  $a.appendChild($descripcion);
+  $a.appendChild($precio);
+})
