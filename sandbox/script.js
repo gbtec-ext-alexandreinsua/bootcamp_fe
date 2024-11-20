206a274d9859e747f909db84010352aec6fa29a4
valeriaterrone <valeriaprogramacioncurso@gmail.com>
2024-11-20 09:49:15 +0100

    ota vez

--- sandbox/script.js
+++ sandbox/script.js
@@ -1,22 +1,7 @@
-// const $padre = document.getElementsByClassName("padre")[0];
-//c onsole.log($padre);
-
-// const timer = setTimeout(function () {
-//   const $primero = document.getElementById("primero");
-//   $primero.classList.add("grande");
-// }, 0);
-
-// $padre.style.flexDirection = "row-reverse";
-// const $divs = document.querySelectorAll(".hijo");
-// console.log($divs);
-
-// for (const div of $divs) {
-  //   div.innerHTML = "<p>Hijo </p>";
- //  div.classList.add("grande");
-// }
 
 const $button = document.getElementById("button"); 
 
 $button.addEventListener("click", function() {
   $button.textContent = "¡Perfecto!";
 });
+
