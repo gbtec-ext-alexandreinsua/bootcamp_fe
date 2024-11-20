0202fd0206cec1627c36e78cfe3a9e17d9221762
Roi Oliveira Outeda <roioliveira@gmail.com>
2024-11-20 11:57:43 +0100

    ejercicio json

--- ejercicios/javascript/06-creacion-elementos-dom/main.js
+++ ejercicios/javascript/06-creacion-elementos-dom/main.js
@@ -8,15 +8,80 @@ a. Crea un archivo products.json con los datos de los productos.
 b. Usa JavaScript para cargar y procesar el JSON.
 c. Genera dinámicamente las tarjetas y añádelas al contenedor .card-wrapper.
 */
-
 const productos = [
   {
     imagen: "./images/producto1.jpg",
     descripcionImagen: "Producto 1",
     nombre: "Producto 1",
     descripcion: "Descripción del producto 1",
-    precio: 10,
+    precio: "$10",
+  },
+  {
+    imagen: "./images/producto2.jpg",
+    descripcionImagen: "Producto 2",
+    nombre: "Producto 2",
+    descripcion: "Descripción del producto 2",
+    precio: "20€",
+  },
+  {
+    imagen: "./images/producto3.jpg",
+    descripcionImagen: "Producto 3",
+    nombre: "Producto 3",
+    descripcion: "Descripción del producto 3",
+    precio: "30€",
+  },
+  {
+    imagen: "./images/producto4.jpg",
+    descripcionImagen: "Producto 4",
+    nombre: "Producto 4",
+    descripcion: "Descripción del producto 4",
+    precio: "24€",
+  },
+  {
+    imagen: "./images/producto5.jpg",
+    descripcionImagen: "Producto 5",
+    nombre: "Producto 5",
+    descripcion: "Descripción del producto 5",
+    precio: "250€",
+  },
+  {
+    imagen: "./images/producto6.jpg",
+    descripcionImagen: "Producto 6",
+    nombre: "Producto 6",
+    descripcion: "Descripción del producto 6",
+    precio: "15€",
   },
 ];
 
-const $wrapper = document.querySelector("card-wrapper");
+
+const $wrapper = document.querySelector(".card-wrapper");
+
+
+for (producto of productos) {
+  const card = document.createElement("div");
+  card.className = "card";
+
+  const enlace = document.createElement("a");
+  enlace.href = "#";
+
+  const imagen = document.createElement("img");
+  imagen.src = producto.imagen;
+  imagen.alt = producto.descripcionImagen;
+
+  const titulo = document.createElement("h3");
+  titulo.textContent = producto.nombre;
+
+  const descripcion = document.createElement("p");
+  descripcion.textContent = producto.descripcion;
+
+  const precio = document.createElement("p");
+  precio.textContent = `Precio: ${producto.precio}`;
+
+  enlace.appendChild(imagen);
+  enlace.appendChild(titulo);
+  enlace.appendChild(descripcion);
+  enlace.appendChild(precio);
+  card.appendChild(enlace);
+
+  $wrapper.appendChild(card);
+}
