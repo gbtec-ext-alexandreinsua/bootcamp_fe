8eb7f1acca02f2282fb8460af931d59bcf7b40e0
jahzie <jahziel.99gl@gmail.com>
2024-11-20 11:40:31 +0100

    Script.js

--- sandbox/script.js
+++ sandbox/script.js
@@ -1,10 +1,25 @@
 const $boton = document.querySelector("button");
 
-
-function mostrarMensaje() {
-  console.log("mensaje");
+function mostrarMensaje(mensaje) {
+    window.alert(mensaje);
 }
 
-$boton.addEventListener("click", mostrarMensaje);
+$boton.addEventListener("click", () => {
+    mostrarMensaje("Hola mundo");
+});
 
-$boton.removeEventListener("click", mostrarMensaje);
+/* ARROW FUNCTIONS */
+
+function sumar(a, b) {
+    return a + b;
+} 
+
+// const sumarA = (a, b) => a + b;
+
+function calcular(a, b, callback) {
+    return callback(a, b);
+}
+
+console.log(calcular(1, 2, sumar));
+
+console.log(calcular(3, 4, (a, b) => a + b));
