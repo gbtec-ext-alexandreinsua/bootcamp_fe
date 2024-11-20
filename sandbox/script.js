33909a8409221092f4dea43f0e8548e13f9a334d
oscardasilvarodriguez <grelodj@hotmail.com>
2024-11-20 11:25:10 +0100

    apuntes css js

--- sandbox/script.js
+++ sandbox/script.js
@@ -1,10 +1,14 @@
 const $padre = document.getElementsByClassName("padre")[0];
 console.log($padre);
 
+<<<<<<< Updated upstream
 // const timer = setTimeout(function () {
 //   const $primero = document.getElementById("primero");
 //   $primero.classList.add("grande");
 // }, 0);
+=======
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
 
 $padre.style.flexDirection = "row-reverse";
 const $divs = document.querySelectorAll(".hijo");
@@ -14,3 +18,19 @@ for (const div of $divs) {
   //   div.innerHTML = "<p>Hijo </p>";
   div.classList.add("grande");
 }
+<<<<<<< Updated upstream
+=======
+
+$boton.addEventListener("click", mostrarMensaje);
+
+$boton.removeEventListener("click", mostrarMensaje);
+=======
+function mostrarMensaje (mensaje) {
+  window.alert(mensaje);
+}
+
+$boton.addEventListener("click", () => {
+  mostrarMensaje("!Hola Mundo¡");
+});
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
