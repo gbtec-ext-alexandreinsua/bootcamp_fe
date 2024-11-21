300ce35717dddf9f6ddffaa244875f920abeb52c
Edugonzalez <eduu.gonzalez@gmail.com>
2024-11-21 10:58:26 +0100

    ejercicio insertar en el DOM

--- ejercicios/javascript/06-creacion-elementos-dom/main.js
+++ ejercicios/javascript/06-creacion-elementos-dom/main.js
@@ -17,6 +17,74 @@ const productos = [
     descripcion: "Descripción del producto 1",
     precio: 10,
   },
+  {
+    imagen: "./images/producto2.jpg",
+    descripcionImagen: "Producto 2",
+    nombre: "Producto 2",
+    descripcion: "Descripción del producto 2",
+    precio: 20,
+  },
+  {
+    imagen: "./images/producto3.jpg",
+    descripcionImagen: "Producto 3",
+    nombre: "Producto 3",
+    descripcion: "Descripción del producto 3",
+    precio: 30,
+  },
+  {
+    imagen: "./images/producto4.jpg",
+    descripcionImagen: "Producto 4",
+    nombre: "Producto 4",
+    descripcion: "Descripción del producto 4",
+    precio: 24,
+  },
+  {
+    imagen: "./images/producto5.jpg",
+    descripcionImagen: "Producto 5",
+    nombre: "Producto 5",
+    descripcion: "Descripción del producto 5",
+    precio: 250,
+  },
+  {
+    imagen: "./images/producto6.jpg",
+    descripcionImagen: "Producto 6",
+    nombre: "Producto 6",
+    descripcion: "Descripción del producto 6",
+    precio: 15,
+  },
 ];
 
-const $wrapper = document.querySelector("card-wrapper");
+const $wrapper = document.querySelector(".card-wrapper");
+
+for(const producto of productos){
+  const card = document.createElement("div");
+  const enlace = document.createElement("a");
+  const imagen = document.createElement("img");
+  const nombre = document.createElement("h3");
+  const descripcion = document.createElement("p");
+  const precio = document.createElement("p");
+
+  
+ 
+  imagen.src = producto.imagen;
+  imagen.alt = producto.descripcionImagen;
+  enlace.setAttribute("href", "#");
+  nombre.innerText = producto.nombre;
+  descripcion.innerText = producto.descripcion;
+  precio.innerText = "Precio: $ " + producto.precio;
+
+  
+  enlace.appendChild(imagen);
+  enlace.appendChild(nombre);
+  enlace.appendChild(descripcion);
+  enlace.appendChild(precio);
+  card.appendChild(enlace);
+
+
+ card.setAttribute("class", "card");
+ $wrapper.appendChild(card);
+
+
+}
+
+
