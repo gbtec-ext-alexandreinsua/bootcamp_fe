df65cd4294f63e4274a700f4eae6094a87d9c534
lorevlmt <lore.vimontes@gmail.com>
2024-11-22 12:29:30 +0100

    wip: continuo ejercicio maquetación

--- ejercicios/html-css/ejercicio-06-maquetacion/script.js
+++ ejercicios/html-css/ejercicio-06-maquetacion/script.js
@@ -1,10 +1,20 @@
-const alerta = [
-    {
-        mensaje: "El nombre del usuario no puede estar en blanco"
-    }
-];
+const banner = document.getElementById("banner");
+const usuarioInput = document.getElementById("usuario");
+const contraseñaInput = document.getElementById("contraseña");
+const botonEntrar = document.getElementById("validar");
 
-const $banner = document.querySelector(".banner");
+// Función para validar y cambiar el mensaje del banner
+function validarDatos() {
+  const usuario = usuarioInput.value.trim();
+  const contrasena = contrasenaInput.value.trim();
 
-const fragment = document.createDocumentFragment();
+  if (!usuario && !contrasena) {
+    banner.textContent = "Usuario desconocido o contraseña incorrecta";
+  } else if (!usuario) {
+    banner.textContent = "El nombre del usuario no puede estar en blanco";
+  } else (!contrasena) {
+    banner.textContent = "La contraseña no puede estar en blanco";
+  }
+}
 
+botonEntrar.addEventListener("click", validarDatos);
