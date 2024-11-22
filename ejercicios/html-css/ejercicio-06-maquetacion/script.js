2f052668c416c30daf8c88e1598b4a6e7599cb05
lorevlmt <lore.vimontes@gmail.com>
2024-11-22 11:49:29 +0100

    wip: continuo ejercicio maquetación

---
+++ ejercicios/html-css/ejercicio-06-maquetacion/script.js
@@ -0,0 +1,10 @@
+const alerta = [
+    {
+        mensaje: "El nombre del usuario no puede estar en blanco"
+    }
+];
+
+const $banner = document.querySelector(".banner");
+
+const fragment = document.createDocumentFragment();
+
