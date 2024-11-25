1e174dd7c4cbd861fd9925171586e93a66d5500f
lorevlmt <lore.vimontes@gmail.com>
2024-11-25 09:43:27 +0100

    ejerciio-06-maquetacion completo

--- ejercicios/html-css/ejercicio-06-maquetacion/script.js
+++ ejercicios/html-css/ejercicio-06-maquetacion/script.js
@@ -1,20 +1,22 @@
 const banner = document.getElementById("banner");
 const usuarioInput = document.getElementById("usuario");
-const contraseñaInput = document.getElementById("contraseña");
-const botonEntrar = document.getElementById("validar");
+const contrasenaInput = document.getElementById("contrasena");
+const botonEntrar = document.getElementById("botonEntrar");
 
-// Función para validar y cambiar el mensaje del banner
+
 function validarDatos() {
   const usuario = usuarioInput.value.trim();
   const contrasena = contrasenaInput.value.trim();
 
+  const alertaSimbolo = "⚠ ";
+
   if (!usuario && !contrasena) {
-    banner.textContent = "Usuario desconocido o contraseña incorrecta";
+    banner.textContent = alertaSimbolo + "Usuario desconocido o contraseña incorrecta";
   } else if (!usuario) {
-    banner.textContent = "El nombre del usuario no puede estar en blanco";
-  } else (!contrasena) {
-    banner.textContent = "La contraseña no puede estar en blanco";
+    banner.textContent = alertaSimbolo + "El nombre del usuario no puede estar en blanco";
+  } else if (!contrasena) {
+    banner.textContent = alertaSimbolo + "La contraseña no puede estar en blanco";
   }
 }
 
-botonEntrar.addEventListener("click", validarDatos);
+botonEntrar.addEventListener('click', validarDatos);
