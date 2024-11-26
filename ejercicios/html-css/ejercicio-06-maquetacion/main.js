65596e2b13eff0e7bfe46064477cf9ba3876bebe
pacarescuba <94604499+pacarescuba@users.noreply.github.com>
2024-11-26 09:41:52 +0100

    bugfix: /ejercicios/html-css/ejercicio-06-maquetacion

--- ejercicios/html-css/ejercicio-06-maquetacion/main.js
+++ ejercicios/html-css/ejercicio-06-maquetacion/main.js
@@ -3,12 +3,10 @@ const $username = document.getElementById("username");
 const $password = document.getElementById("password");
 let formData;
 
-document.getElementsByClassName("null-password").style.display = "flex";
-
 $entrar.addEventListener("click", function () {
   console.log("ButtonPress")
-  const $nullPassword = document.getElementsByClassName("null-password");
-  const $badPassword = document.getElementsByClassName("bad-password");
+  const $nullPassword = document.getElementById("null-password");
+  const $badPassword = document.getElementById("bad-password");
   if (!$username) {
     $nullPassword.style.display = "flex";
   } else {
