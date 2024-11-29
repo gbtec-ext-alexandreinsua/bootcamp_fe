7434e252036008ce9d56e0a33fa31819dd0cafff
Jorge338991 <jorge338991@gmail.com>
2024-11-29 10:10:32 +0100

    anvances v1.3 con ejercicio->javascript->08-dom-y-fetch

--- ejercicios/javascript/08-dom-y-fetch/js/asincronia.js
+++ ejercicios/javascript/08-dom-y-fetch/js/asincronia.js
@@ -7,6 +7,9 @@ async function pedirFoto() {
 
         //console.log(foto);
         //console.log(foto.url);
+        if(!foto.ok){
+            throw new Error("Esta fallando");
+        }
         return foto.json();
 
 
@@ -18,45 +21,45 @@ async function pedirFoto() {
 async function main() {
     const resultado = await pedirFoto();
     const urlImg = resultado.results[0].picture.large; // JSON
-    const p = "My name is ";
-    const h1 = resultado.results[0].name.title + " " + resultado.results[0].name.first + " " + resultado.results[0].name.last;
-    const h2 = resultado.results[0].location.city + " " + resultado.results[0].location.street.name + " " + resultado.results[0].location.street.number;
-    const h3 = resultado.results[0].phone + " " + resultado.results[0].email;
+    const p2 = "My name is ";
+    const h1_2 = resultado.results[0].name.title + " " + resultado.results[0].name.first + " " + resultado.results[0].name.last;
+    const h2_2 = resultado.results[0].location.city + " " + resultado.results[0].location.street.name + " " + resultado.results[0].location.street.number;
+    const h3_2 = resultado.results[0].phone + " " + resultado.results[0].email;
    
     
 
     
     
-    const $card = document.createElement("div");
-        $card.classList.add("card");
-    const $subCard1 = document.createElement("div");
-        $subCard1.classList.add("subCard1");
-        const $img = document.createElement("img");
-            $img.src=urlImg;
+    const card = document.createElement("div");
+        card.classList.add("card");
+    const subCard1 = document.createElement("div");
+        subCard1.classList.add("subCard1");
+        const img = document.createElement("img");
+            img.src=urlImg;
     
-    const $subCard2 = document.createElement("div");
-        $subCard2.classList.add("subCard2");
-    const $p = document.createElement("p");
-        $p.innerText = p;
-    const $h1 = document.createElement("h1");
-        $h1.innerText = h1;
-    const $h2 = document.createElement("h2");
-        $h2.innerText = h2;
-    const $h3 = document.createElement("h3");
-        $h3.innerText = h3;
+    const subCard2 = document.createElement("div");
+        subCard2.classList.add("subCard2");
+    const p = document.createElement("p");
+        p.innerText = p2;
+    const h1 = document.createElement("h1");
+        h1.innerText = h1_2;
+    const h2 = document.createElement("h2");
+        h2.innerText = h2_2;
+    const h3 = document.createElement("h3");
+        h3.innerText = h3_2;
 
-    $subCard1.appendChild($img)
+    subCard1.appendChild(img)
 
-    $subCard2.appendChild($p);
-    $subCard2.appendChild($h1);
-    $subCard2.appendChild($h2);
-    $subCard2.appendChild($h3);
+    subCard2.appendChild(p);
+    subCard2.appendChild(h1);
+    subCard2.appendChild(h2);
+    subCard2.appendChild(h3);
 
-    $card.appendChild($subCard1);
-    $card.appendChild($subCard2);
+    card.appendChild(subCard1);
+    card.appendChild(subCard2);
 
     
-    window.document.querySelector("body").appendChild($card);
+    window.document.querySelector("body").appendChild(card);
 
 
 
