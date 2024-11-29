05e1e8b4f9dea5bb1d079233eee28f30ab9eb7fa
Roi Oliveira Outeda <roioliveira@gmail.com>
2024-11-29 11:07:11 +0100

    Ejercicio con una modificacion del DOM

--- ejercicios/javascript/08-dom-y-fetch/08-1-randomuser/specs/dom-y-fetch.js
+++ ejercicios/javascript/08-dom-y-fetch/08-1-randomuser/specs/dom-y-fetch.js
@@ -1,32 +1,41 @@
 const API_URL = "https://randomuser.me/api/";
 
+let usuarioCargado = false;
+
 async function fetchUserData() {
   try {
     const response = await fetch(API_URL);
     if (!response.ok) {
-      throw new Error(`Error en la API ! Status: ${response.status}`);
+      throw new Error(`Error en la API!: ${response.status}`);
     }
     const data = await response.json();
     const user = data.results[0];
 
-    
-    document.getElementById("user-photo").src = user.picture.large;
-    document.getElementById("user-name").textContent = `${user.name.first} ${user.name.last}`;
-    document.getElementById("user-address").textContent = `${user.location.city} - ${user.location.street.name}, ${user.location.street.number}`;
-    document.getElementById("user-phone").textContent = user.phone;
-    document.getElementById("user-email").textContent = user.email;
+    if (!usuarioCargado) {
+      const cardContent = `
+        <div class="card-header">
+          <img src="${user.picture.large}" alt="User Photo">
+        </div>
+        <div class="card-body">
+          <p>Hi, My name is</p>
+          <h2>${user.name.first} ${user.name.last}</h2>
+          <p>${user.location.city} - ${user.location.street.name}, ${user.location.street.number}</p>
+          <p>${user.phone} - ${user.email}</p>
+        </div>
+      `;
+      document.querySelector(".card").innerHTML = cardContent;
+
+      usuarioCargado = true;
+    }
   } catch (error) {
-    
-    throw new Error(`Error fetch : ${error.message}`);
+    console.error(`Error en fetchUserData: ${error.message}`);
   }
 }
 
-
-(async function initcializar() {
+(async function inicializar() {
   try {
     await fetchUserData();
   } catch (error) {
-    console.error("Error al inicializar :", error);
-   
+    console.error("Error al inicializar:", error);
   }
 })();
