bd5cb16ffbb8f3995f25f66de33e9c39cfbf2114
Alfonso Vidal <vidaltorres.alfonso@gmail.com>
2024-12-12 12:31:40 +0100

    ejercicio 28 excepciones

--- ejercicios/javascript/07-excepciones/07-excepciones.js
+++ ejercicios/javascript/07-excepciones/07-excepciones.js
@@ -8,6 +8,62 @@ Crea una función espedífica  que divida dos números y lanza una excepción si
 Usa try-catch para manejar el error en otra función. 
  */
 
+const $dividir = document.getElementById("dividir");
+
+
+function dividir(num1, num2){
+    if(!Number(num2)){
+        throw new Error("No se puede dividir entre 0");
+    }
+    return (num1 / num2).toFixed(2);
+}
+
+$dividir.addEventListener("click", function(event){
+    event.preventDefault();
+    
+    function getDatosFormulario(){
+        num1 = $dividendo.value.trim();
+        num2 = $divisor.value.trim();
+        num1Int = Number(num1);
+        num2Int = Number(num2);
+        if (!num1 || !num2){
+            return {
+                correcto: false,
+                mensaje: "Error. Introduce ambos inputs."
+            };
+        } else if(Number.isNaN(num1) ||Number.isNaN(num2)){
+            return {
+                correcto: false,
+                mensaje: "Error. Los campos deben ser números."
+            };
+        } else{
+            return {
+                correcto: true,
+                dividendo: num1Int,
+                divisor: num2Int
+            };
+        }
+    }
+
+    const $dividendo = document.getElementById("dividendo");
+    const $divisor = document.getElementById("divisor");
+    const $respuesta = document.getElementById("respuesta");
+    const datosFormulario = getDatosFormulario();
+    console.log(datosFormulario);
+    if (datosFormulario.correcto){
+        try {
+            $respuesta.innerText = `La division de ${datosFormulario.dividendo} entre ${datosFormulario.divisor} es igual a ${dividir(datosFormulario.dividendo, datosFormulario.divisor)}.`;
+        } catch (error) {
+            $respuesta.innerText = error;
+        }
+    } else{
+        $respuesta.innerText = datosFormulario.mensaje;
+    }
+
+});
+
+
+
 /* EJERCICIO 29.-
 Crea un formulario para que el usuario pueda introducir un array de números enteros.
 Hay que validar que todos los valores sean números válidos.
@@ -15,6 +71,8 @@ El mensaje de no haber introducido nada debe ser diferente de un números invál
 Escribe una función que acepte un array y lance una excepción de tipo RangeError si el tamaño del array es mayor que 10.
 Debe mostar la media de la lista,
 */
+
+
 
 /* EJERCICIO 30.- 
 Crea una web que solicite al usuario un valor numérico. 
