69c5158c61d9c9943130c3c830d177dfb67e2d6c
Jorge338991 <jorge338991@gmail.com>
2024-12-12 12:06:04 +0100

    Se soluciona ejercicio 28 ejercicios/javascript/07-excepciones

--- ejercicios/javascript/07-excepciones/07-excepciones.js
+++ ejercicios/javascript/07-excepciones/07-excepciones.js
@@ -7,6 +7,48 @@ un div.
 Crea una función espedífica  que divida dos números y lanza una excepción si el divisor es cero.
 Usa try-catch para manejar el error en otra función. 
  */
+function procesarEjercicio28() {
+  try {
+    const divEjercicio28 = document.getElementById("ejercicio28");
+    const dividendo = Math.floor(
+      Number(document.getElementById("dividendo").value)
+    );
+    const divisor = Math.floor(
+      Number(document.getElementById("divisor").value)
+    );
+
+    if (isNaN(dividendo) || isNaN(divisor)) {
+      throw new Error("Por favor, ingresa valores numéricos.");
+    }
+
+    if (!divisor) {
+      throw new Error("El divisor no puede ser 0.");
+    }
+
+    const resultado = dividendo / divisor;
+
+    console.log(
+      `Dividendo: ${dividendo}, Divisor: ${divisor}, Resultado: ${resultado}`
+    );
+    window.alert(
+      `Dividendo: ${dividendo}, Divisor: ${divisor}, Resultado: ${resultado}`
+    );
+    const resultadoFinal =  `${dividendo} / ${divisor} = Resultado: ${resultado}`;
+    window.alert(resultado);
+    const $p = document.createElement("p");
+    $p.textContent = resultadoFinal;
+    $p.style.color = "green";
+    divEjercicio28.appendChild($p);
+  } catch (error) {
+    window.alert(error.message);
+    console.error(error.message);
+    
+  }
+}
+
+// Asociar el evento click al botón
+const $enviar28 = document.getElementById("enviar28");
+$enviar28.addEventListener("click", procesarEjercicio28);
 
 /* EJERCICIO 29.-
 Crea un formulario para que el usuario pueda introducir un array de números enteros.
@@ -15,6 +57,15 @@ El mensaje de no haber introducido nada debe ser diferente de un números invál
 Escribe una función que acepte un array y lance una excepción de tipo RangeError si el tamaño del array es mayor que 10.
 Debe mostar la media de la lista,
 */
+
+let $enviar29 = document.getElementById("enviar29");
+$enviar29.addEventListener("click", procesarEjercicio29);
+
+function procesarEjercicio29() {
+  /*1,2,3,4,5,6,7,8,9,0*/
+  alert("hola29");
+  const myArray = document.getElementById("miArray");
+}
 
 /* EJERCICIO 30.- 
 Crea una web que solicite al usuario un valor numérico. 
