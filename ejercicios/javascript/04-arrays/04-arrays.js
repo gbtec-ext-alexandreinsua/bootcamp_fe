246c6ee016aef8d3e26a5aeab38a6a64fba9c3eb
oscardasilvarodriguez <grelodj@hotmail.com>
2025-01-07 09:18:11 +0100

    hasta enero

--- ejercicios/javascript/04-arrays/04-arrays.js
+++ ejercicios/javascript/04-arrays/04-arrays.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-import { esPrimo } from "../../../js/math-utils.js";
->>>>>>> development
 
 /*
 EJERCICIO 21.- 
@@ -82,7 +78,6 @@ crea una función que devuelva otro con los números pares. Se puede usar un
 bucle for o un bucle for-of.
 */
 
-<<<<<<< HEAD
 
 // function soloPares(numbers) {
 
@@ -101,26 +96,6 @@ bucle for o un bucle for-of.
 // }
 
 // console.log(soloPares(numbers));
-=======
-function filtrarPares(numeros) {
-  const pares = new Array();
-  for (const numero of numeros) {
-    if (numero % 2 === 0) {
-      pares.push(numero);
-    }
-  }
-  return pares;
-}
-// const numbers = [
-//   50, 93, 62, 2, 66, 17, 94, 46, 32, 83, 52, 10, 86, 72, 11, 68, 9, 37, 26, 20,
-// ];
-
-function filtrarParesFuncional(numeros) {
-  return numeros.filter((n) => !(n % 2));
-}
-
-// console.log(filtrarPares(numbers));
->>>>>>> development
 // console.log(filtrarParesFuncional(numbers));
 
 /* EJERCICIO 23.-
@@ -129,26 +104,6 @@ const numbers = [50,93,62,2,66,17,94,46,32,83,52,10,86,72,11,68,9,37,26,20]
 crea una función que devuelva otro con el doble de cada número primo de la primera
 Se puede usar un bucle for o un bucle for-of.
 */
-const numbers = [
-  50, 93, 62, 2, 66, 17, 94, 46, 32, 83, 52, 10, 86, 72, 11, 68, 9, 37, 26, 20,
-];
-
-function duplicarPrimo(numeros) {
-  const primosDuplicados = new Array();
-
-  for (const numero of numeros) {
-    if (esPrimo(numero)) {
-      primosDuplicados.push(numero * 2);
-    }
-  }
-  return primosDuplicados;
-}
-
-function duplicarPrimoFuncional(numeros) {
-  return numeros.filter(esPrimo).map((numero) => numero * 2);
-}
-// console.log(duplicarPrimo(numbers));
-// console.log(duplicarPrimoFuncional(numbers));
 
 
 
@@ -221,110 +176,26 @@ function descubrirSecretoFuncional(array) {
   console.log(result2);
 
 
-function descubrirSecreto(array) {
-  let secreto = "";
-  for (const palabra of array) {
-    secreto += palabra.charAt(0);
-  }
-  return secreto;
-}
-
-function descubrirSecretoFuncional(array) {
-  return array.reduce((secreto, palabra) => {
-    secreto += palabra.charAt(0);
-    return secreto;
-  }, "");
-}
-
-const palabras = [
-  "Yate",
-  " ",
-  "tigre",
-  "elefante",
-  "nube",
-  "elección",
-  "montaña",
-  "oso",
-  "sol",
-  " ",
-  "agua",
-  "quijote",
-  "uva",
-  "ícara",
-  ",",
-  " ",
-  "mango",
-  "esfera",
-  ",",
-  " ",
-  "dado",
-  "iguana",
-  "sol",
-  "cisne",
-  "ulises",
-  "luna",
-  "pato",
-  "oso",
-  " ",
-  "magnolia",
-  "agua",
-  "granizo",
-  "nieve",
-  "oso",
-  "llama",
-  "icono",
-  "agua",
-  "sapo",
-  " ",
-  "quijote",
-  "uva",
-  "eucalipto",
-  " ",
-  "diamante",
-  "enano",
-  "viento",
-  "osa",
-  "río",
-  "agua",
-  "nilo",
-  " ",
-  "isla",
-  "nada",
-  "serpiente",
-  "espacio",
-  "cuerda",
-  "tarde",
-  "oso",
-  "sándalo",
-  " ",
-  "cisne",
-  "oso",
-  "nube",
-  " ",
-  "sol",
-  "urna",
-  "sierra",
-  " ",
-  "llama",
-  "enigma",
-  "nube",
-  "granizo",
-  "urna",
-  "alondra",
-  "sol",
-  ".",
-];
-// console.log(descubrirSecreto(palabras));
-// console.log(descubrirSecretoFuncional(palabras));
-
 /* EJERCICIO 25.- 
 Usuarios de una biblioteca.
 Necesitamos tener en una lista aquellos usuarios de la biblioteca que devuelven sus préstamos a tiempo
 porque queremos premiarlos.
 Crea un método que muestre la lista usando el punto medio (·) como viñeta y el salto de línea \n.
 Aquí tienes la lista de todos los usuarios.
+
+const socios = [
+  { "nombre": "Juan", "apellido": "Pérez", "sancionado": false },
+  { "nombre": "María", "apellido": "González", "sancionado": true },
+  { "nombre": "Luis", "apellido": "Rodríguez", "sancionado": false },
+  { "nombre": "Ana", "apellido": "Martínez", "sancionado": true },
+  { "nombre": "Carlos", "apellido": "López", "sancionado": false },
+  { "nombre": "Laura", "apellido": "Sánchez", "sancionado": true },
+  { "nombre": "José", "apellido": "García", "sancionado": false },
+  { "nombre": "Elena", "apellido": "Torres", "sancionado": true },
+  { "nombre": "Pedro", "apellido": "Vázquez", "sancionado": false },
+  { "nombre": "Sandra", "apellido": "Jiménez", "sancionado": true }
+];
 */
-<<<<<<< HEAD
 
 const socios = [
   { "nombre": "Juan", "apellido": "Pérez", "sancionado": false },
@@ -342,72 +213,11 @@ const socios = [
 function mostrarUsuariosPremiados(socios) {
   const usuariosPremiados = socios
     .filter(socio => !socio.sancionado) 
-    .map(socio => `· ${socio.nombre} ${socio.apellido} \n`); 
+    .map(socio => `· ${socio.nombre} ${socio.apellido}`) 
+    .join('\n'); 
   
   return usuariosPremiados;
 }
 
 const listaPremiados = mostrarUsuariosPremiados(socios);
 console.log(listaPremiados);
-=======
-const socios = [
-  { nombre: "Juan", apellido: "Pérez", sancionado: false },
-  { nombre: "María", apellido: "González", sancionado: true },
-  { nombre: "Luis", apellido: "Rodríguez", sancionado: false },
-  { nombre: "Ana", apellido: "Martínez", sancionado: true },
-  { nombre: "Carlos", apellido: "López", sancionado: false },
-  { nombre: "Laura", apellido: "Sánchez", sancionado: true },
-  { nombre: "José", apellido: "García", sancionado: false },
-  { nombre: "Elena", apellido: "Torres", sancionado: true },
-  { nombre: "Pedro", apellido: "Vázquez", sancionado: false },
-  { nombre: "Sandra", apellido: "Jiménez", sancionado: true },
-];
-
-function filtrarUsuariosPuntuales(usuarios) {
-  const usuariosPuntuales = new Array();
-
-  for (const usuario of usuarios) {
-    if (!usuario.sancionado) {
-      usuariosPuntuales.push(usuario);
-    }
-  }
-  return usuariosPuntuales;
-}
-
-function mostrarUsuarios(usuarios) {
-  let result = "";
-  for (const usuario of usuarios) {
-    result += `· ${usuario.nombre} ${usuario.apellido} \n`;
-  }
-  return result;
-}
-
-function mostrarUsuariosPuntuales(usuarios) {
-  window.alert(
-    `LOS USUARIOS PUNTUALES SON: \n` +
-      mostrarUsuarios(filtrarUsuariosPuntuales(usuarios))
-  );
-}
-
-function filtrarUsuariosPuntualesFuncional(usuarios) {
-  return usuarios.filter((usuario) => !usuario.sancionado);
-}
-
-function mostrarUsuariosFuncional(usuarios) {
-  return usuarios.reduce(
-    (string, usuario) =>
-      (string += `· ${usuario.nombre} ${usuario.apellido} \n`),
-    ""
-  );
-}
-
-function mostrarUsuariosPuntualesFuncional(usuarios) {
-  window.alert(
-    `LOS USUARIOS PUNTUALES SON: \n` +
-      mostrarUsuariosFuncional(filtrarUsuariosPuntualesFuncional(usuarios))
-  );
-}
-
-// mostrarUsuariosPuntuales(socios);
-// mostrarUsuariosPuntualesFuncional(socios)
->>>>>>> development
--- ejercicios/javascript/07-excepciones/07-excepciones.js
+++ ejercicios/javascript/07-excepciones/07-excepciones.js
@@ -7,6 +7,46 @@ un div.
 Crea una función espedífica  que divida dos números y lanza una excepción si el divisor es cero.
 Usa try-catch para manejar el error en otra función. 
  */
+const dividirBtn = document.getElementById('dividir');
+dividirBtn.addEventListener("click", dividirException);
+
+function dividir(dividendo, divisor){
+    if (!divisor){
+      throw new Error ("No se puede dividir entre 0")
+    }
+    return dividendo / divisor;
+  }
+
+function dividirException() {
+    const num1Input = document.getElementById('num1');
+    const num2Input = document.getElementById('num2');
+    const num1InputR = num1Input.value.trim();
+    const num2InputR = num2Input.value.trim();
+    const resultDiv = document.getElementById('result');
+    const errorDiv = document.getElementById('error');
+
+    resultDiv.textContent = '';
+    errorDiv.textContent = '';
+
+    try {       
+        if (!num1InputR || !num2InputR) {
+            throw new Error('Ambos campos son obligatorios.');
+        }
+        const num1 = parseFloat(num1InputR);
+        const num2 = parseFloat(num2InputR);
+        if (isNaN(num1) || isNaN(num2)) {
+            throw new Error('Por favor, introduce números válidos.');
+        }
+        const result = dividir(num1, num2);
+        resultDiv.textContent = `${num1InputR} / ${num2InputR} = ${result}`;
+        
+    } catch (error) {
+        errorDiv.textContent = error.message;
+    } 
+    num1Input.value= '';
+    num2Input.value= '';
+}
+
 
 /* EJERCICIO 29.-
 Crea un formulario para que el usuario pueda introducir un array de números enteros.
@@ -16,6 +56,40 @@ Escribe una función que acepte un array y lance una excepción de tipo RangeErr
 Debe mostar la media de la lista,
 */
 
+const $calcularMediaBtn = document.getElementById("calcularMediaBtn");
+
+$calcularMediaBtn.addEventListener("click", function() {
+    const $result29 = document.getElementById("result29");
+    // recuperar el dato del formulario
+    const $arrayInput = document.getElementById("array");
+    const arrayString = $arrayInput.value.trim(); // "1,2,3,4,5"
+    let media = 0;
+
+    try {
+        media = handleArray(transformStringIntoArray(arrayString));
+    } catch (e) {
+        $result29.textContent = `Error`;
+        console.error(e);
+    }
+
+    $result29.textContent = `La media es ${media}.`;
+});
+
+    // convertirlo a un array de numeros
+function transformStringIntoArray(string) {
+    return array = string.split(",").map((char) => {
+        const num =Number.parseInt(char, 10));
+}
+
+function handleArray(numbers) {
+    const maxNumbers = 10;
+    if (maxNumbers < numbers.length) {
+        throw new RangeError("El tamaño del array es demasiado grande");
+    }
+    return numbers.reduce((suma, numero)=> (suma += numero), 0)/ numbers.length;
+}
+
+
 /* EJERCICIO 30.- 
 Crea una web que solicite al usuario un valor numérico. 
 La app debe devolver el cuadrado de ese número y su factorial si el número es menor de 10.
@@ -23,3 +97,33 @@ Si es mayor de 10, solo se muestra el resultado.
 El mensaje de no haber introducido nada debe ser diferente de haber ingresado un número inválido.
 Si el valor ingresado no es un número, lanza una excepción personalizada y manéjala en un bloque try-catch.
 */
+const $enviar30 = document.getElementById("enviar30");
+
+$enviar30.addEventListener("click", procesarEj30);
+
+function procesarEj30 () {
+    const cuadOFact = 10;
+    const stringValue = document.getElementById("numero").trim();
+    if(!stringValue) {
+        window.alert("No es valida una cadena vacia");
+    } else {
+        const number = Number(stringValue)
+        console.log(number);
+        
+        if(number > cuadOFact) {
+            window.alert("es mayor que 10");
+
+        }
+    }
+}
+
+function cuadrado (number) {
+    return number * number
+}
+
+function factorial (number) {
+
+    return
+}
+const numero = document.getElementById("numero").value;
+
